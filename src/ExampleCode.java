5fd6a3bdda3a7bea13573954b0b072c105eca339
hinda_bukshpan <hynda4@gmail.com>
2024-06-20 21:44:39 +0300

    "primitive variable"

--- src/ExampleCode.java
+++ src/ExampleCode.java
@@ -1,9 +1,25 @@
 public class ExampleCode {
     public static void main(String[] args) {
 //        System.out.println("sout");
-        for (int i = 0; i <= args.length; i++) {
+        for (int i = 1; i <= args.length; i++) {
             System.out.println(args[i]);
         }
+
+    int num;
+        num =2;
+    Object object = null;
+    char sigh = '@';
+
+
+        System.out.println(object);
+        System.out.println(num);
+        System.out.println(sigh);
+
+
+
+
+
+
     }
 
 }
