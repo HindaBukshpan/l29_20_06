8f495f71b4508992593fef541f607af592e19087
hinda_bukshpan <hynda4@gmail.com>
2024-06-20 21:17:43 +0300

    "first commit"

---
+++ src/ExampleCode.java
@@ -0,0 +1,9 @@
+public class ExampleCode {
+    public static void main(String[] args) {
+//        System.out.println("sout");
+        for (int i = 0; i <= args.length; i++) {
+            System.out.println(args[i]);
+        }
+    }
+
+}
